b704e28a1986c2900dde4e9b7a15ce9cc9c615f1
Maksim Makarenko <makmakale87@gmail.com>
2024-02-13 21:26:05 +0300

    deploy to render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,50 +4,42 @@ import { defineConfig } from 'vite';
 import { createStyleImportPlugin } from 'vite-plugin-style-import';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    base: '/',
-    optimizeDeps: {
-      include: ['react-quill'],
-    },
-    plugins: [
-      react({
-        include: /\.(jsx|tsx)$/,
-        babel: {
-          plugins: ['styled-components'],
-          babelrc: false,
-          configFile: false,
+export default defineConfig(() => ({
+  base: '/',
+  optimizeDeps: {
+    include: ['react-quill'],
+  },
+  plugins: [
+    react({
+      include: /\.(jsx|tsx)$/,
+      babel: {
+        plugins: ['styled-components'],
+        babelrc: false,
+        configFile: false,
+      },
+    }),
+    createStyleImportPlugin({
+      libs: [
+        {
+          libraryName: 'react-quill',
+          resolveStyle: (name) => `react-quill/dist/quill.${name}.css`,
         },
-      }),
-      createStyleImportPlugin({
-        libs: [
-          {
-            libraryName: 'react-quill',
-            resolveStyle: (name) => `react-quill/dist/quill.${name}.css`,
-          },
-        ],
-      }),
-    ],
-    server: {
-      port: 3000,
-      proxy: {
-        '/api': {
-          target: 'http://localhost:5000',
-          changeOrigin: true,
-        },
+      ],
+    }),
+  ],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
       },
     },
-    resolve: {
-      alias: {
-        '@': `${path.resolve(__dirname, 'src')}`,
-      },
-      extensions: ['.js', '.jsx'],
+  },
+  resolve: {
+    alias: {
+      '@': `${path.resolve(__dirname, 'src')}`,
     },
-  };
-
-  if (command !== 'serve') {
-    config.base = '/todo-board-uue7.onrender.com/';
-  }
-
-  return config;
-});
+    extensions: ['.js', '.jsx'],
+  },
+}));
