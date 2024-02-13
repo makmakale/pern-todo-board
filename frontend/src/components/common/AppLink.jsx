d45d88519589a07b26bb43114c34efb645dc1be1
Maksim Makarenko <makmakale87@gmail.com>
2024-02-13 18:03:39 +0300

    resolve react version conflicts

--- frontend/src/components/common/AppLink.jsx
+++ frontend/src/components/common/AppLink.jsx
@@ -1,26 +1,10 @@
 import { Link as MuiLink } from '@mui/material';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import makeStyles from '@mui/styles/makeStyles';
 import { Link as RouterLink } from 'react-router-dom';
 
-const useStyles = makeStyles((theme) => ({
-  link: {
-    textDecoration: 'none',
-    color: theme.palette.primary.main,
-    '&:hover': {
-      color: theme.palette.primary.dark,
-    },
-  },
-}));
-
-const theme = createTheme();
-
 function CustomLink(props) {
-  const classes = useStyles();
-
   return (
     <RouterLink {...props}>
-      <MuiLink className={classes.link} {...props}>
+      <MuiLink {...props}>
         {/* eslint-disable-next-line react/destructuring-assignment */}
         {props.title}
       </MuiLink>
@@ -28,12 +12,4 @@ function CustomLink(props) {
   );
 }
 
-function AppLink(props) {
-  return (
-    <ThemeProvider theme={theme}>
-      <CustomLink {...props} />
-    </ThemeProvider>
-  );
-}
-
-export default AppLink;
+export default CustomLink;
